cffa6d20b1c2f87f3cce44148f5a45f88f08339c
ouafid <mohamedhabchi@teccart.online>
2025-01-21 11:43:18 -0500

    update notifications v1

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,6 +1,5 @@
+// utils/notifications.js
 const { Expo } = require('expo-server-sdk');
-const axios = require('axios');
-
 const expo = new Expo();
 
 async function sendPushNotification(expoPushToken, message) {
@@ -9,30 +8,18 @@ async function sendPushNotification(expoPushToken, message) {
         return;
     }
 
-    const messages = [
-        {
-            to: expoPushToken,
-            sound: 'default',
-            body: message.body || 'Notification par défaut',
-            title: message.title || 'Titre par défaut',
-            data: message.data || { withSome: 'data' },
-        },
-    ];
+    const messages = [{
+        to: expoPushToken,
+        sound: 'default',
+        body: message,
+        data: { withSome: 'data' },
+    }];
 
     try {
-        // Envoi via le SDK local Expo
         let tickets = await expo.sendPushNotificationsAsync(messages);
-        console.log('Notification envoyée avec succès via SDK Expo:', tickets);
+        console.log('Notification envoyée avec succès:', tickets);
     } catch (error) {
-        console.error('Erreur d\'envoi de la notification via SDK Expo:', error);
-    }
-
-    try {
-        // Envoi via l'API Expo
-        const response = await axios.post('https://exp.host/--/api/v2/push/send', messages[0]);
-        console.log('Notification envoyée avec succès via l\'API Expo:', response.data);
-    } catch (error) {
-        console.error('Erreur lors de l\'envoi de la notification via l\'API Expo:', error.response?.data || error.message);
+        console.error('Erreur d\'envoi de la notification:', error);
     }
 }
 
